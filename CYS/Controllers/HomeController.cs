eb9e1224acaadb603cc7be7dad16f9e86a41f56a
Muhammed <muhammed.eminoglu@selcuk.edu.tr>
2023-02-23 14:34:37 +0300

    a

--- CYS/Controllers/HomeController.cs
+++ CYS/Controllers/HomeController.cs
@@ -37,11 +37,7 @@ namespace CYS.Controllers
 			return View();
 		}
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
+		
 
 		bool sessionKontrol()
 		{
