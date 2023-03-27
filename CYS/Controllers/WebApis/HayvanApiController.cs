b83302482bf74c2ac908748e60c98511a7ff6edf
Muhammed Eminoglu <meminoglu@windowslive.com>
2023-03-28 01:56:43 +0300

    f

---
+++ CYS/Controllers/WebApis/HayvanApiController.cs
@@ -0,0 +1,20 @@
+﻿using CYS.Models;
+using CYS.Repos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CYS.Controllers.WebApis
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HayvanApiController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<Hayvan> Get(int userId)
+		{
+			HayvanCTX hctx = new HayvanCTX();
+			return hctx.hayvanList("select * from Hayvan where userId = @userId", new {userId = userId});
+		}
+
+	}
+}
