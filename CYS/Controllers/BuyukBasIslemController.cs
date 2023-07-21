1ef2939e7d783088b148b62a2131b70205c5b58b
muhammed.eminoglu@selcuk.edu.tr <muhammed.eminoglu@selcuk.edu.tr>
2023-07-21 09:08:20 +0300

    .

--- CYS/Controllers/BuyukBasIslemController.cs
+++ CYS/Controllers/BuyukBasIslemController.cs
@@ -1,5 +1,6 @@
 ﻿using CYS.Repos;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Runtime.InteropServices;
 
 namespace CYS.Controllers
@@ -17,7 +18,26 @@ namespace CYS.Controllers
 			var mevcut = processsettingCTX.processsettingTek("select * from processsetting where id = 1", null);
 			mevcut.islemModu = 121;
 			processsettingCTX.processtypeGuncelle(mevcut);
+
+			Thread.Sleep(1000);
+			mevcut.islemModu = -1;
+			mevcut.mevcutRequest = "";
+			processsettingCTX.processtypeGuncelle(mevcut);
+
 			return Json("1");
+		}
+
+		public JsonResult olcum(string requestId)
+		{
+			agirliksuCTX agirliksuCTX = new agirliksuCTX();
+			var mevcut = agirliksuCTX.agirliksuTek("select * from agirliksu where requestId = @requestId", new {requestId = requestId});
+			if (mevcut == null)
+				return Json("");
+			else
+			{
+				var gonderilecek = JsonConvert.SerializeObject(mevcut);
+				return Json(gonderilecek);
+			}
 		}
 	}
 }
