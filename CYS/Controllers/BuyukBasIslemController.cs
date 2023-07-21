bdffbae0968521a5010bb97952d0dfc2fbcce37f
muhammed.eminoglu@selcuk.edu.tr <muhammed.eminoglu@selcuk.edu.tr>
2023-07-21 08:49:20 +0300

    .

---
+++ CYS/Controllers/BuyukBasIslemController.cs
@@ -0,0 +1,23 @@
+﻿using CYS.Repos;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace CYS.Controllers
+{
+	public class BuyukBasIslemController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public JsonResult iptalEt()
+		{
+			processsettingCTX processsettingCTX = new processsettingCTX();
+			var mevcut = processsettingCTX.processsettingTek("select * from processsetting where id = 1", null);
+			mevcut.islemModu = 121;
+			processsettingCTX.processtypeGuncelle(mevcut);
+			return Json("1");
+		}
+	}
+}
