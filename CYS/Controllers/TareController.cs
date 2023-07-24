e8afb493e7bbf718d1a107cbc81fcc3ec616ad0c
muhammed.eminoglu@selcuk.edu.tr <muhammed.eminoglu@selcuk.edu.tr>
2023-07-24 14:39:52 +0300

    rer

---
+++ CYS/Controllers/TareController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CYS.Controllers
+{
+	public class TareController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult KantarAyari()
+		{
+			if (sessionKontrol() == false)
+				return RedirectToAction("GirisYap", "Login");
+			return View();
+		}
+
+		bool sessionKontrol()
+		{
+
+			try
+			{
+
+				var user = HttpContext.Session.GetString("user");
+				var profile = HttpContext.Session.GetString("profile");
+
+				if (user == null)
+					return false;
+				return true;
+			}
+			catch (Exception ex)
+			{
+				return false;
+			}
+		}
+
+	}
+}
